ec6672c5c2ccc1d7b5b5000bbe9f0c48785ec3ff
David van Erkelens <me@davidvanerkelens.nl>
2017-12-04 19:46:58 +0100

    Store triplets, running code now

--- pipeline/download.py
+++ pipeline/download.py
@@ -4,9 +4,13 @@ import pypandoc
 import lxml.etree as ET
 from bs4 import BeautifulSoup
 import re
+import pickle
 
 # Main function
 def main():
+
+    # Load word IDs
+    word2id = pickle.load(open('../data/wiki/id2token.p', 'rb'))
 
     # Open file with Wikipedia triplets, downloaded from http://cs.stanford.edu/ ̃quocle/triplets-data.tar.gz
     with open('wikipedia_2014_09_27_examples.txt') as file:
@@ -17,6 +21,9 @@ def main():
         # Number of processed triplets
         processed = 0
 
+        # Store the dataset
+        dataset = []
+
         # Loop over lines
         for line in contents:
 
@@ -25,7 +32,7 @@ def main():
                 continue
 
             # Number of processed articles
-            article = 0
+            num_articles = 0
 
             # Save categories per article
             linecats = {}
@@ -33,8 +40,17 @@ def main():
             # Is this line valid (i.e. do all articles exist and are they parsable)?
             validline = True
 
+            # Store triple
+            triple = {}
+
+            # Store articles
+            triple['articles'] = {}
+
             # Loop over articles
             for item in line.split():
+
+                # Store article
+                article = {}
 
                 # Get XML export URL
                 url = item.replace('http://en.wikipedia.org/wiki/', 'https://en.wikipedia.org/wiki/Special:Export/')
@@ -70,31 +86,59 @@ def main():
                 cats = re.findall('\[\[Category:(.+?)\]\]', content)
 
                 # Store a set of the articles
-                linecats[article] = set(cats)
-
-                # We've processed one more article
-                article += 1
+                linecats[num_articles] = set(cats)
 
                 # Cleanup the last bits of HTML from the plain text
                 clean = BeautifulSoup(converted, "lxml").text
 
-                # TODO: Save & tokenize file using existing code
+                # Store text
+                article['text'] = clean
+
+                # Store URL
+                article['url'] = item
+
+                # Store tokenized article
+                tokenized = [word2id[x] if x in word2id else 0 for x in clean.split()]
+                article['tokens'] = tokenized
+                triple['articles'][num_articles] = article
+
+                # We've processed one more article
+                num_articles += 1
+
             
             # Skip further processing of this line if it is not valid
             if not validline:
                 continue
 
-            # Debug prints comparing categories
-            print("1 & 2:")
-            print(list(linecats[0] & linecats[1]))
-            print("2 & 3:")
-            print(list(linecats[1] & linecats[2]))
-            print("1 & 3:")
-            print(list(linecats[0] & linecats[2]))
-            print("\n\n----------\n\n")
+            # Get the outlier
+            onetwo = list(linecats[0] & linecats[1])
+            twothree = list(linecats[1] & linecats[2])
+            onethree = list(linecats[0] & linecats[2])
+
+            # Is there one set with more overlap?
+            if len(onetwo) == len(twothree) == len(onethree): 
+                continue
+
+            # which one is the outlier?
+            outlier = 0
+
+            if len(onetwo) > len(twothree) and len(onetwo) > len(onethree):
+                outlier = 2
+
+            if len(twothree) > len(onetwo) and len(twothree) > len(onethree):
+                outlier = 1
+
+            # Store outlier
+            triple['outlier'] = outlier
+
+            # Save it
+            pickle.dump(triple, open('../data/wiki/triples/' + str(processed) + '.p', 'wb'))
 
             # We've processed one more line
             processed += 1
+
+            # Print
+            print("Stored triplet " + str(processed))
 
             # Break for testing purposes
             if processed > 50:
