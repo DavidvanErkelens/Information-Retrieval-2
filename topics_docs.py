7959a8f60aa0a7253bf1cf57ca87b67b3b681d17
trdavidson <tim@lytit.com>
2017-12-16 16:50:56 +0100

    added topic clustering option for non geo-vec models

--- topics_docs.py
+++ topics_docs.py
@@ -19,7 +19,8 @@ def get_avg_f1(y_true, y_pred):
         
         if l in pred_labels:
             g2_scores = [f1_score(np.where(y_true == c, 1, 0), 
-                                  np.where(y_pred == l, 1, 0)) for c in pred_labels]
+                                  np.where(y_pred == l, 1, 0)) 
+                                  for c in pred_labels]
             g2 = np.max(g2_scores) / len(pred_labels)
             avg_f1_score += g2
         
@@ -29,12 +30,15 @@ def get_avg_f1(y_true, y_pred):
     
     return avg_f1_score
 
-def topic_stats(embeddings, y_true, num_classes):
+def topic_stats(embeddings, y_true, num_classes, geo_vec=False):
     """ Computes topic cluster statistics for document vectors
     Param:
-        embeddings: (D, B, C) tensor, where D is num documents, BxD final embedding
+        embeddings: (geo_vec), (D, B, C) tensor, where D is num documents, 
+                                BxD final embedding.
+                    (D, C) tensor, where D is num documents, C embedding.
         y_true: (D, 1), correct topic per document
         num_classes: total number of classes in the dataset 
+        geo_vec: check the simple topic activation
     Returns:
         f1_s: weighted f1 score
         ari: adjusted rand score
@@ -42,16 +46,20 @@ def topic_stats(embeddings, y_true, num_classes):
         nmi: normalized mutual information score
         topic_s: maxed topic activations
     """
-    topic_act = np.argmax(np.sum(embeddings, axis=2), axis=1)
-    embeddings = np.reshape(c, (np.shape(c)[0], -1))
+    if geo_vec:
+        topic_act = np.argmax(np.sum(embeddings, axis=2), axis=1)
+        embeddings = np.reshape(c, (np.shape(c)[0], -1))
+
     kmeans = KMeans(n_clusters=num_classes, random_state=0).fit(embeddings)
     
     f1_s, ari, ami, nmi, topic_s = 0, 0, 0, 0, 0
     def score(y_true, y_pred, name):
         n = ['avg_f1', 'adj_ri', 'adj_mi', 'norm_mi', 'topic_act']
         s = [get_avg_f1(y_true, y_pred), adjusted_rand_score(y_true, y_pred), 
-              adjusted_mutual_info_score(y_true, y_pred),normalized_mutual_info_score(y_true, y_pred),
-              accuracy_score(y_true, topic_act)]
+              adjusted_mutual_info_score(y_true, y_pred),
+              normalized_mutual_info_score(y_true, y_pred),float('NaN')]
+        if geo_vec:
+            s[-1] = (accuracy_score(y_true, topic_act))
         print ('      %10s  %10s  %10s  %10s  %10s \n %3s %8.3f  %10.3f  %10.3f  %10.3f  %10.3f\n' % (
                n[0],n[1],n[2],n[3],n[4],name,s[0],s[1],s[2],s[3], s[4]))
         return s
@@ -67,8 +75,11 @@ def topic_stats(embeddings, y_true, num_classes):
 
 a = np.array([[[0, 5],[2, 2]]])
 b = np.array([[[0, 1],[2, 2]]])
+# geo_vec model example
 c = np.vstack((a, b))
+# regular doc embedding example
+#c = np.squeeze(a)
 cl = np.array([0, 1])
-topic_stats(c, cl, 2)
+topic_stats(c, cl, 2, True)
 
 
